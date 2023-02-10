d64badc5e7f051fda9e3dfc1d0168a4a1dafe5b0
amirjavani <amirjavani@outlook.com>
2023-02-11 01:45:35 +0330

    fixing bug of login

--- frontend/lib/ProfilePage.dart
+++ frontend/lib/ProfilePage.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:online_shop/NewProductPage.dart';
+import 'package:online_shop/main.dart';
 import 'package:online_shop/user.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -10,6 +12,7 @@ class ProfilePage extends StatefulWidget {
   @override
   State<ProfilePage> createState() => _ProfilePageState();
   static bool logedIn = false;
+  static bool isAdmin = false;
   static User user =
       User('email', 'fullName', 'city', 'phone', 'password', 'address');
 }
@@ -17,10 +20,18 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    if (ProfilePage.logedIn) {
-      return Userprofile(onLogedIn: () {
-        setState(() {});
-      });
+    if (ProfilePage.logedIn && !ProfilePage.isAdmin) {
+      return Userprofile(
+        onLogedIn: () {
+          setState(() {});
+        },
+      );
+    } else if (ProfilePage.logedIn && ProfilePage.isAdmin) {
+      return AdminProfile(
+        onLogedIn: () {
+          setState(() {});
+        },
+      );
     } else {
       return LoginPage(
         onLogedIn: () {
@@ -28,6 +39,69 @@ class _ProfilePageState extends State<ProfilePage> {
         },
       );
     }
+  }
+}
+
+class AdminProfile extends StatefulWidget {
+  const AdminProfile({Key? key, required this.onLogedIn}) : super(key: key);
+  final Function onLogedIn;
+  @override
+  State<AdminProfile> createState() => _AdminProfileState(this.onLogedIn);
+}
+
+class _AdminProfileState extends State<AdminProfile> {
+  Function _function;
+  _AdminProfileState(this._function);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(120),
+                child: Image.asset(
+                  'asset/image/profile.png',
+                  width: 120,
+                  height: 120,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  ProfilePage.isAdmin = false;
+                  ProfilePage.logedIn = false;
+                  _function();
+                },
+                child: const Text('logout'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return NewProductPage();
+                      },
+                    ),
+                  );
+                },
+                child: const Text('add new product'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -193,6 +267,15 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  bool isEmpty = true;
+  void empty() {
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      isEmpty = false;
+    } else {
+      isEmpty = true;
+    }
+  }
+
   final snackBarError = const SnackBar(
       content: Text('password or Email was wrong'),
       backgroundColor: Colors.redAccent,
@@ -283,21 +366,28 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             ElevatedButton(
-              onPressed: () {
-                // final Uri url = Uri.parse("http://192.168.135.63:8000/login");
-                // final headers = {'Content-Type': 'application/json'};
-                // final response = await http.post(url,
-                //     headers: headers,
-                //     body: json.encode({
-                //       "customerEmail": emailController.text,
-                //       "password": passwordController.text
-                //     }));
-                // var decoded = json.decode(response.body);
-                // print(decoded['isLogin']);
+              onPressed: () async {
+                final Uri url = Uri.parse("${MyApp.url}/login");
+                final headers = {'Content-Type': 'application/json'};
+                final response = await http.post(url,
+                    headers: headers,
+                    body: json.encode({
+                      "customerEmail": emailController.text,
+                      "password": passwordController.text
+                    }));
+                var decoded = json.decode(response.body);
+                print(decoded['isLogin']);
+                empty();
                 setState(() {
                   firstTime = false;
-                  if (emailController.text == 'amir' &&
-                      passwordController.text == '123') {
+                  if (decoded['isLogin'] && !isEmpty) {
+                    _function();
+                    ProfilePage.logedIn = true;
+                    ScaffoldMessenger.of(context).showSnackBar(snackBarSuccess);
+                  } else if (emailController.text == 'amir' &&
+                      passwordController.text == '12' &&
+                      isAdmin) {
+                    ProfilePage.isAdmin = true;
                     _function();
                     ProfilePage.logedIn = true;
                     ScaffoldMessenger.of(context).showSnackBar(snackBarSuccess);
@@ -387,8 +477,56 @@ class _SignUpState extends State<SignUp> {
   TextEditingController fullNameController = TextEditingController();
 
   TextEditingController cityController = TextEditingController();
+
   final Function functionChangeLogedIn;
+
   _SignUpState(this.functionChangeLogedIn);
+
+  bool firstTime = true;
+
+  String? errorHandeler(String text) {
+    if (text.isEmpty) {
+      return 'required';
+    } else {
+      return null;
+    }
+  }
+
+  bool isEmpty = true;
+
+  void empty() {
+    if (usernameController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty &&
+        phoneController.text.isNotEmpty &&
+        addressController.text.isNotEmpty &&
+        fullNameController.text.isNotEmpty &&
+        cityController.text.isNotEmpty) {
+      isEmpty = false;
+    } else {
+      isEmpty = true;
+    }
+  }
+
+  final snackBarError = const SnackBar(
+      content: Text('can not use this email'),
+      backgroundColor: Colors.redAccent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+        side: BorderSide(
+          color: Colors.red,
+        ),
+      ));
+  final snackBarSuccess = const SnackBar(
+      content: Text('sign up success'),
+      backgroundColor: Colors.greenAccent,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(10), topLeft: Radius.circular(10)),
+        side: BorderSide(
+          color: Colors.green,
+        ),
+      ));
 
   @override
   Widget build(BuildContext context) {
@@ -412,6 +550,8 @@ class _SignUpState extends State<SignUp> {
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.email),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(usernameController.text),
                   labelText: 'Email',
                   hintText: 'Enter Your Email',
                   contentPadding: EdgeInsets.all(20),
@@ -424,6 +564,8 @@ class _SignUpState extends State<SignUp> {
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.lock_outline),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(passwordController.text),
                   labelText: 'Password ',
                   hintText: 'Enter Password',
                   contentPadding: EdgeInsets.all(20),
@@ -435,6 +577,8 @@ class _SignUpState extends State<SignUp> {
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.house),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(addressController.text),
                   labelText: 'ADRRESS',
                   hintText: 'enter your address',
                   contentPadding: EdgeInsets.all(20),
@@ -446,6 +590,8 @@ class _SignUpState extends State<SignUp> {
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.person),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(fullNameController.text),
                   labelText: 'FULLNAME',
                   hintText: 'enter your full name',
                   contentPadding: EdgeInsets.all(20),
@@ -457,47 +603,63 @@ class _SignUpState extends State<SignUp> {
                 decoration: InputDecoration(
                   prefixIcon: Icon(Icons.phone_android),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(phoneController.text),
                   labelText: 'PHONE',
                   hintText: 'enter your phone number',
                   contentPadding: EdgeInsets.all(20),
                 ),
               ),
-              SizedBox(height: 10),
+              SizedBox(
+                height: 10,
+              ),
               TextField(
                 controller: cityController,
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                   prefixIcon: Icon(Icons.location_city),
                   border: OutlineInputBorder(),
+                  errorText:
+                      firstTime ? null : errorHandeler(cityController.text),
                   labelText: 'CITY',
                   hintText: 'enter your city',
                   contentPadding: EdgeInsets.all(20),
                 ),
               ),
               ElevatedButton(
-                onPressed: () {
-                  // // _futureAlbum =
-                  // //     createAlbum(usernameController.text) as Future<Album>?;
-                  // // final Uri url = Uri.parse("http://10.0.2.2:8000/signup");
-                  // final Uri url =
-                  //     Uri.parse("http://192.168.135.63:8000/signup");
-                  // final headers = {'Content-Type': 'application/json'};
-                  // final response = await http.post(url,
-                  //     headers: headers,
-                  //     body: json.encode({
-                  //       "customerFullName": fullNameController.text,
-                  //       'phone': phoneController.text,
-                  //       'city': cityController.text,
-                  //       'address': addressController.text,
-                  //       'customerEmail': usernameController.text,
-                  //       "password": passwordController.text
-                  //     }));
-                  // var decoded = json.decode(response.body);
+                onPressed: () async {
+                  final Uri url = Uri.parse("${MyApp.url}/signup");
+                  final headers = {'Content-Type': 'application/json'};
+                  final response = await http.post(url,
+                      headers: headers,
+                      body: json.encode({
+                        "customerFullName": fullNameController.text,
+                        'phone': phoneController.text,
+                        'city': cityController.text,
+                        'address': addressController.text,
+                        'customerEmail': usernameController.text,
+                        "password": passwordController.text
+                      }));
+                  var decoded = json.decode(response.body);
+                  empty();
                   setState(() {
-                    ProfilePage.logedIn = true;
-                    functionChangeLogedIn();
+                    firstTime = false;
+                    if (!decoded["isExistEmail"] && !isEmpty) {
+                      ProfilePage.logedIn = true;
+                      functionChangeLogedIn();
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(snackBarSuccess);
+                      ProfilePage.user = User(
+                          usernameController.text,
+                          fullNameController.text,
+                          cityController.text,
+                          phoneController.text,
+                          passwordController.text,
+                          addressController.text);
+                      Navigator.pop(context);
+                    } else if (!isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(snackBarError);
+                    }
                   });
-
-                  Navigator.pop(context);
                 },
                 child: const Text(
                   "Sign up",
